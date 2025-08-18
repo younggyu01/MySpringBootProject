1324c0dc695ae7aad590fa35889cc8e9b2d6f25a
younggyu01 <gugy0706@gmail.com>
2025-08-18 11:07:22 +0900

    index.html daa

--- src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
@@ -7,6 +7,7 @@ import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -38,10 +39,7 @@ public class UserRestController {
     //ID로 조회
     @GetMapping("/{id}")
     public UserEntity getUser(@PathVariable Long id){
-        Optional<UserEntity> optionalUser = userRepository.findById(id);
-        //orElseThrow(Supplier) Supplier의 추상메서드 T get()
-        UserEntity existUser = optionalUser
-                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+        UserEntity existUser = getExistUser(id);
         return existUser;
     }
     //Email로 조회하고, 수정
@@ -54,6 +52,21 @@ public class UserRestController {
         //DB에 저장
         UserEntity updateUser = userRepository.save(existUser);
         return updateUser;
+    }
+    //삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id){
+        UserEntity existUser = getExistUser(id);
+        //DB에 삭제요청
+        userRepository.delete(existUser);
+        return ResponseEntity.ok("User가 삭제되었습니다.");
+    }
+
+    private UserEntity getExistUser(Long id) {
+        Optional<UserEntity> optionalUser = userRepository.findById(id);
+        UserEntity existUser = optionalUser
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+        return existUser;
     }
 
 }
