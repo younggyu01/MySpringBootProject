c135626b79e4030a5ceed428d69513a0f116a191
younggyu01 <gugy0706@gmail.com>
2025-08-18 17:22:41 +0900

    error add

--- src/main/java/com/rookies4/myspringboot/entity/UserEntity.java
+++ src/main/java/com/rookies4/myspringboot/entity/UserEntity.java
@@ -1,6 +1,9 @@
 package com.rookies4.myspringboot.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -18,9 +21,12 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message = "Name은 필수 입력항목입니다!")
     @Column(nullable = false)
     private String name;
 
+    @NotBlank(message = "Email은 필수 입력항목입니다!")
+    @Email
     @Column(unique = true, nullable = false)
     private String email;
 
