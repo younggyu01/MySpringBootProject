120ac85c30c33856aa79d0134726b25ae890db90
younggyu01 <gugy0706@gmail.com>
2025-08-27 16:04:52 +0900

    Department add

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-### [수업중 2-4] Spring Boot와 JPA(Java Persistence API) 활용
+### [수업중 2-5] Spring Boot와 JPA(Java Persistence API) 활용
 
 * Student 와 StudentDetail 1:1 (OneToOne) 엔티티 연관관계
+* Student 와 Department 1:N (OneToMany) 엔티티 연관관계
     * FetchType.LAZY vs FetchType.EAGER
     * @JoinColumn, mappedBy
     * 연관관계의 주인(owner 와 종속(non-owner)
@@ -9,3 +10,7 @@
 * Controller
 * Service
 * Repository
+* DataInsertRunner
+* N+1 문제 해결
+  * 성능 개선
+  * HibernateModule 사용하여 BatchSize 설정하기
---
+++ src/main/java/com/rookies4/myspringboot/entity/Department.java
@@ -0,0 +1,41 @@
+package com.rookies4.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "department_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String code;
+
+    private List<Student> students = new ArrayList<>();
+
+    // Helper methods
+    public void addStudent(Student student) {
+        students.add(student);
+        student.setDepartment(this);
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+        student.setDepartment(null);
+    }
+}
